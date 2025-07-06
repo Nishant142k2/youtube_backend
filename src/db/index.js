c39a0b9cea997a169ee1b06fcc9f8d988bf544b5
Nishant <ayushnishantbro1@gmail.com>
2025-07-07 01:38:07 +0530

    db connected

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+ import mongoose from "mongoose";
+ import { DB_NAME } from "../constants.js";
+
+ const connectToDatabase = async () => {
+    try {
+       const connectionInstance =await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`Connected to MongoDB at ${connectionInstance.connection.host}:${connectionInstance.connection.port}/${DB_NAME}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+ }
+
+export default connectToDatabase;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require("dotenv").config()
+import dotenv from "dotenv";
+import connectToDatabase from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+connectToDatabase()
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express ;
+// (async ()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", (error) => {
+//         console.log("Error in Express app:", error);
+//         throw error
+//        })
+//        app.listen(process.env.PORT || 3000, () => {
+//         console.log(`Server is running on port ${process.env.PORT || 3000}`);
+//        });
+//     } catch(error) {
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// })()
